ae3002a326e3d024bded43b9164149e8923e1bfc
Yo Lo <iwen2kastarito@gmail.com>
2025-02-14 00:08:03 +0330

    fifteenth commit, getting ready for connectings. connection desing done

--- ConnectionsControl.xaml.cs
+++ ConnectionsControl.xaml.cs
@@ -15,7 +15,7 @@ namespace EchoOrbit.Controls
         {
             InitializeComponent();
             DataContext = this;
-            // Sample data; replace with actual live data.
+            // Sample data for online users.
             OnlineUsers = new ObservableCollection<OnlineUser>
             {
                 new OnlineUser { DisplayName = "Alice", ProfileImage = new BitmapImage(new Uri("pack://application:,,,/defaultProfile.png", UriKind.Absolute)) },
@@ -24,12 +24,71 @@ namespace EchoOrbit.Controls
                 new OnlineUser { DisplayName = "David", ProfileImage = new BitmapImage(new Uri("pack://application:,,,/defaultProfile.png", UriKind.Absolute)) }
             };
 
-            Groups = new ObservableCollection<Group>
+            // Start with no groups.
+            Groups = new ObservableCollection<Group>();
+
+            // Populate NewGroupMembersListBox with OnlineUsers for selection.
+            NewGroupMembersListBox.ItemsSource = OnlineUsers;
+        }
+
+        private void NewGroupButton_Click(object sender, RoutedEventArgs e)
+        {
+            NewGroupPanel.Visibility = (NewGroupPanel.Visibility == Visibility.Visible) ? Visibility.Collapsed : Visibility.Visible;
+        }
+
+        private void NewGroupChangeImageButton_Click(object sender, RoutedEventArgs e)
+        {
+            Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
+            dlg.Filter = "Image Files|*.png;*.jpg;*.jpeg;*.bmp";
+            if (dlg.ShowDialog() == true)
             {
-                new Group { GroupName = "Developers", GroupImage = new BitmapImage(new Uri("pack://application:,,,/defaultGroup.png", UriKind.Absolute)) },
-                new Group { GroupName = "Designers", GroupImage = new BitmapImage(new Uri("pack://application:,,,/defaultGroup.png", UriKind.Absolute)) },
-                new Group { GroupName = "Managers", GroupImage = new BitmapImage(new Uri("pack://application:,,,/defaultGroup.png", UriKind.Absolute)) }
-            };
+                NewGroupImagePreview.Source = new BitmapImage(new Uri(dlg.FileName, UriKind.Absolute));
+                NewGroupImagePreview.Tag = dlg.FileName;
+            }
+        }
+
+        private void CancelNewGroupButton_Click(object sender, RoutedEventArgs e)
+        {
+            NewGroupPanel.Visibility = Visibility.Collapsed;
+        }
+
+        private void CreateGroupButton_Click(object sender, RoutedEventArgs e)
+        {
+            string groupName = NewGroupNameTextBox.Text;
+            if (string.IsNullOrWhiteSpace(groupName))
+            {
+                MessageBox.Show("Please enter a group name.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+            string imagePath = NewGroupImagePreview.Tag as string;
+            BitmapImage groupImage;
+            if (!string.IsNullOrEmpty(imagePath) && System.IO.File.Exists(imagePath))
+            {
+                groupImage = new BitmapImage(new Uri(imagePath, UriKind.Absolute));
+            }
+            else
+            {
+                groupImage = new BitmapImage(new Uri("pack://application:,,,/defaultGroup.png", UriKind.Absolute));
+            }
+            Group newGroup = new Group { GroupName = groupName, GroupImage = groupImage };
+            Groups.Add(newGroup);
+            MessageBox.Show("New group created.", "Group", MessageBoxButton.OK, MessageBoxImage.Information);
+            NewGroupPanel.Visibility = Visibility.Collapsed;
+            NewGroupNameTextBox.Text = "";
+            NewGroupImagePreview.Source = null;
+            NewGroupImagePreview.Tag = null;
+            NewGroupMembersListBox.SelectedItems.Clear();
+        }
+
+        private void RemoveGroupButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (sender is Button btn && btn.DataContext is Group group)
+            {
+                if (MessageBox.Show($"Are you sure you want to remove group '{group.GroupName}'?", "Confirm Removal", MessageBoxButton.YesNo, MessageBoxImage.Question) == MessageBoxResult.Yes)
+                {
+                    Groups.Remove(group);
+                }
+            }
         }
     }
 
---
+++ Models/Group.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+using System.Windows.Media.Imaging;
+
+namespace EchoOrbit.Models
+{
+    public class Group
+    {
+        public string GroupName { get; set; }
+        public BitmapImage GroupImage { get; set; }
+        public ObservableCollection<OnlineUser> Members { get; set; } = new ObservableCollection<OnlineUser>();
+    }
+}
